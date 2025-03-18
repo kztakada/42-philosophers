87225c581a9737c4a6bd89747c1461d5e9e97404
katakada <katakada@student.42tokyo.jp>
2025-03-18 16:53:37 +0900

    Add initial files for philo project: Makefile, header, and main source

---
+++ philo/src/main.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 16:27:51 by katakada          #+#    #+#             */
+/*   Updated: 2025/03/18 16:37:32 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
